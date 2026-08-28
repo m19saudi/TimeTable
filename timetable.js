0b041827265e21f9fc4a618f0078b3bdc4bcf6b9
m19saudi <269192418+m19saudi@users.noreply.github.com>
2026-08-28 15:08:00 +0300

    Update timetable.js

--- timetable.js
+++ timetable.js
@@ -1,9 +1,35 @@
 const MASTER_TIMETABLE = {
-    "Sun-0": {
-        "subject": "Computer Science",
-        "room": "Room 153",
-        "instructor": "Dr. Smith",
-        "link": "",
+    "Tue-4": {
+        "subject": "English II",
+        "room": "EL112",
+        "instructor": "احمد زكي",
+        "color": "green"
+    },
+    "Tue-0": {
+        "subject": "Computing Essentials",
+        "room": "TU170",
+        "instructor": "سهى الشيخ",
         "color": "blue"
+    },
+    "Sun-4": {
+        "subject": "Calculus and Probability",
+        "room": "MT129",
+        "instructor": "احمد ابوبكر",
+        "link": "",
+        "color": "crimson"
+    },
+    "Mon-4": {
+        "subject": "Arabic I",
+        "room": "AR111",
+        "instructor": "فهد مفتاح الفهمي",
+        "link": "",
+        "color": "purple"
+    },
+    "Wed-0": {
+        "subject": "Self-Learning Skills",
+        "room": "GR101",
+        "instructor": "هادي العجمي",
+        "link": "",
+        "color": "teal"
     }
 };
