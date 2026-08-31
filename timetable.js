bf6519ef3eb3fcbccb3e7cd7a7c882ae1766b129
m19saudi <269192418+m19saudi@users.noreply.github.com>
2026-08-31 18:25:53 +0300

    Update timetable.js

--- timetable.js
+++ timetable.js
@@ -1,35 +1,37 @@
 const MASTER_TIMETABLE = {
     "Tue-4": {
         "subject": "English II",
-        "room": "EL112",
+        "room": "EL112 - 1305",
         "instructor": "احمد زكي",
+        "link": "https://teams.microsoft.com/meet/346380068264251?p=gXVoUCSmjg5a7LmzyZ",
         "color": "green"
     },
     "Tue-0": {
         "subject": "Computing Essentials",
-        "room": "TU170",
+        "room": "TU170 - 6301",
         "instructor": "سهى الشيخ",
+        "link": "https://u18526086.ct.sendgrid.net/ls/click?upn=u001.sxvE2J0vyOdHkfI6l-2F7cUij1FxGW0jNyEB9MqW9xkLZDGA6YzAo17-2F7Nbxrr8F0r_Vwb_PqQJQerriPb8PeXjXT18YvKjF1Ut9uzJ1EaO9Hw1U5g588fVXBWg8EU40K-2F-2FuExyJspsaXurBUN-2FCnXWXs23lpWGhqRfeQs2B5BtADUIvfetFvYgfULDWVZQKbEIsvfWGwCyhmR5Dmei3gN9eoep-2BN0F3lQWe2bTWJFc42l9KUnmuNehqhr4547quwTWZP-2BM71ExllaRWSbEzJtsrw0TfA-3D-3D",
         "color": "blue"
     },
     "Sun-4": {
         "subject": "Calculus and Probability",
-        "room": "MT129",
+        "room": "MT129 - 3301",
         "instructor": "احمد ابوبكر",
-        "link": "",
+        "link": "https://teams.microsoft.com/meet/37394138189816?p=tiBEkkCCwweWSSMWLr",
         "color": "crimson"
     },
     "Mon-4": {
         "subject": "Arabic I",
-        "room": "AR111",
+        "room": "AR111 - 1375",
         "instructor": "فهد مفتاح الفهمي",
-        "link": "",
+        "link": "https://teams.microsoft.com/meet/381018405666117?p=sffMTtDYH420OM7phb",
         "color": "purple"
     },
     "Wed-0": {
         "subject": "Self-Learning Skills",
-        "room": "GR101",
+        "room": "GR101 - 1303",
         "instructor": "هادي العجمي",
-        "link": "",
+        "link": "https://teams.microsoft.com/meet/396633550156142?p=YrT3jHG0N4Had7bZnq",
         "color": "teal"
     }
 };
